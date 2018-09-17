a8813d592413a3d1a1841c6ad192f4bf2f549a20
Glenn Svanberg <signeratsvanberg@gmail.com>
2018-09-17 22:45:13 +0200

    Add Birthdate To Customer

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -12,6 +12,6 @@ namespace Vidly.Models
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
         public string Name { get; set; }
-
+        
     }
 }
