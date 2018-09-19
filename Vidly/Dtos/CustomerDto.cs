951b67a5f96203bf62fe83912c56eded70f2b21b
Glenn Svanberg <signeratsvanberg@gmail.com>
2018-09-19 14:37:51 +0200

    Configure API to use Dto and AutoMapper

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -12,18 +12,14 @@ namespace Vidly.Dtos
 
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Please enter customer's name.")]
+        [Required]
         [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSuscribedToNewsletter { get; set; }
 
-        public MembershipType MembershipType { get; set; }
-
-        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
-        [Display(Name = "Date of Birth")]
         [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
