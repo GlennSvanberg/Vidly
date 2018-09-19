32dc4766c78b7e2aa2ada6859aba34a1303948c1
Glenn Svanberg <signeratsvanberg@gmail.com>
2018-09-19 11:19:29 +0200

    Added edit and new movie options

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,22 @@ namespace Vidly.Models
 {
     public class Movie
     {
+
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
+    
         public Genre Genre { get; set; }
+
+        [Required]
         public int GenreId { get; set; }
     }
 }
